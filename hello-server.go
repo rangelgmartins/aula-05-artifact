5e15263e046d4b366a91cbb39b8d96ff00a747b9
rangelgmartins <rangel.martins@aluno.faculdadeimpacta.com.br>
2024-09-05 13:06:08 -0300

    Update hello-server.go

--- hello-server.go
+++ hello-server.go
@@ -1,13 +1,26 @@
 package main
+
 import (
     "fmt"
+    "log"
     "net/http"
 )
 
 func main() {
     http.HandleFunc("/", HelloServer)
-    http.ListenAndServe(":13000", nil)
+    
+    // Adicionando verificação de erro na inicialização do servidor
+    err := http.ListenAndServe(":13000", nil)
+    if err != nil {
+        log.Fatalf("Erro ao iniciar o servidor: %v", err)
+    }
 }
+
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+    // Adicionando uma verificação simples para um path vazio
+    if r.URL.Path == "/" {
+        fmt.Fprintf(w, "Hello, World!")
+    } else {
+        fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+    }
 }
